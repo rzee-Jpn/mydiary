4749afbcdeb7941fc63f0e4abdf52a1e5f2a9633
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2025-12-21 22:29:48 +0800

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,10 +1,8 @@
-const bar = document.querySelector('#progressBar span');
-const text = document.getElementById('progressText');
-
-window.addEventListener('scroll', () => {
-  const h = document.documentElement;
-  const percent = (h.scrollTop / (h.scrollHeight - h.clientHeight)) * 100;
-  bar.style.width = percent + '%';
-  text.textContent = Math.round(percent) + '%';
-  localStorage.setItem('progress', percent);
+window.addEventListener("scroll", () => {
+  const doc = document.documentElement;
+  const percent = Math.round(
+    (doc.scrollTop / (doc.scrollHeight - doc.clientHeight)) * 100
+  );
+  document.getElementById("progressText").textContent = percent + "%";
+  document.getElementById("progressBar").style.width = percent + "%";
 });
