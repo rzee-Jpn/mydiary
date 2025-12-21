35e14f6f84b1610a29ce049b68d8ababcd0840cf
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2025-12-21 10:11:03 +0800

    Add functionality for settings and theme adjustments

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,14 @@
+const toc = document.getElementById('toc');
+const settings = document.getElementById('settings');
 
+menuBtn.onclick = ()=> toc.classList.toggle('show');
+settingBtn.onclick = ()=> settings.classList.toggle('show');
+
+fontSize.oninput = e=>{
+  document.documentElement.style
+    .setProperty('--font-size', e.target.value + 'px');
+};
+
+themeSelect.onchange = e=>{
+  document.body.className = 'theme-' + e.target.value;
+};
