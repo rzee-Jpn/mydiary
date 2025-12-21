ec0722b6113d83bb369a82f0b103713645487f9a
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2025-12-21 10:28:28 +0800

    Update tts.js

--- js/tts.js
+++ js/tts.js
@@ -1,15 +1,17 @@
 const synth = speechSynthesis;
 
-document.querySelectorAll('#reader p').forEach(p=>{
-  p.onclick = ()=>{
-    synth.cancel();
-    document.querySelectorAll('.highlight')
-      .forEach(x=>x.classList.remove('highlight'));
+function attachTTS() {
+  document.querySelectorAll("#reader p").forEach(p => {
+    p.onclick = () => {
+      synth.cancel();
+      document.querySelectorAll(".highlight")
+        .forEach(x => x.classList.remove("highlight"));
 
-    const u = new SpeechSynthesisUtterance(p.innerText);
-    u.lang = 'id-ID';
-    p.classList.add('highlight');
-    u.onend = ()=>p.classList.remove('highlight');
-    synth.speak(u);
-  };
-});
+      const u = new SpeechSynthesisUtterance(p.textContent);
+      u.lang = "id-ID";
+      p.classList.add("highlight");
+      u.onend = () => p.classList.remove("highlight");
+      synth.speak(u);
+    };
+  });
+}
