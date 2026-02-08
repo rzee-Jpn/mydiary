763841844defb8108ab1228784f835b1a9327871
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-02-08 18:38:35 +0800

    Create data/Alexa.js

---
+++ gadipakai/data/Alexa.js
@@ -0,0 +1,550 @@
+// AUTO-GENERATED — DO NOT EDIT
+
+export const band = "Alexa";
+
+export const songs = [
+  {
+    title: `Sampai Mati`,
+    body: `
+ [ intro ]  E  
+ 
+E        A    B      E 
+entah di mana dirimu berada 
+G#m        C#    A               G#m 
+tlah ku mencari hingga ke ujung dunia 
+    A   B    E 
+kau ada di mana 
+ 
+E        A  B           E 
+bila kau di sana merasa bahagia 
+G#m     C#   A           G#m 
+aku kan rela arungi luas samudera 
+  A     B    E 
+untukmu di sana 
+ 
+E 
+engkau di sana dan di sana kan tetap di sana 
+    F#m                 B 
+dan aku di sini menyendiri dan tetap di sini 
+   G#m          C#m          A 
+sampai nanti sampai nanti sampai nanti 
+  B                E 
+kita kan bertemu lagi 
+ 
+ [ int ]  E  
+ 
+E 
+engkau di sana dan di sana kan tetap di sana 
+    F#m                 B 
+dan aku di sini menyendiri dan tetap di sini 
+   G#m           C#m 
+sampai nanti sampai nanti  
+    F#m          G#m 
+sampai nanti sampai nanti 
+    F#m          G#m 
+sampai nanti sampai nanti 
+    F#m       
+sampai nanti  
+  B 
+kita kan bertemu lagi 
+ 
+ [ ending ]  E
+`
+  },
+  {
+    title: `Dewi`,
+    body: `
+ E                  F#m       G#m       F#m                E 
+kutanamkan hatiku ,  tumbuh bersamamu 
+E                  F#m       G#m       F#m                E 
+takkan kupetik hingga akhir masa hidupku 
+C#m7             D 
+dengarlah . .  kau dengar 
+C#m7                     D                  A             E 
+selama bumi berputar ,  ku tetap milikmu 
+ 
+E                     C#m7                 F#m 
+dewi . .  bukalah kedua matamu 
+ *                                        B 
+pandanglah ruang di hatiku 
+E                     C#m7               F#m 
+dewi . .  berikan nafasmu untukku 
+ *                                      B 
+agar kuhirup bersamamu 
+Am             G#m               F#m     E    F#m  G#m F#m E 
+bersamamu terus bersamamu 
+ 
+ *  1 - - x 
+   2 - - 7 
+   3 - - 6                  G tau nama chord nya .  yg kiri senar yg kanan grid nya 
+   4 - - 5 
+   5 - - x 
+   6 - - 6
+`
+  },
+  {
+    title: `Jangan Kau Lepas`,
+    body: `
+          E          B               C#m 
+peluklah diriku dan jangan kau lepaskanku 
+    B               A     B   E 
+dan jangan kau lepaskanku darimu 
+ 
+                      E 
+ku takkan pernah tertawa 
+                       B 
+ku takkan pernah bahagia 
+                     C#m 
+ku takkan pernah merasakannya 
+                A   B 
+bila kau tak di sini 
+                      E 
+ku takkan pernah tertawa 
+                       B 
+ku takkan pernah sempurna 
+                     C#m 
+ku takkan pernah merasakanmu 
+                A    B 
+bila kau tak di sini 
+ 
+A               B 
+oh . .  izinkan aku berlutut mengharap  
+E       B    C#m B 
+kau tuk kembali 
+A          B 
+izinkan aku berharap dirimu  
+A  
+kembali dan kembali 
+    B 
+dan kembali lagi 
+ 
+ [ chorus ] 
+         E          B               C#m 
+peluklah diriku dan jangan kau lepaskanku 
+    B               A     B   E 
+dan jangan kau lepaskanku darimu 
+         E          B               C#m 
+peluklah diriku dan jangan kau lepaskanku 
+    B               A     B   E 
+dan jangan kau lepaskanku darimu
+`
+  },
+  {
+    title: `Jangan pernah pergi`,
+    body: `
+ [ intro ]  E 
+ 
+E 
+ bersama rinduku bersama rindumu 
+C#m 
+ bersama hatiku warnai langkahmu 
+A 
+ bersama denganmu arungi mimpiku 
+ 
+E 
+ 
+ [ chorus ] 
+B              A 
+ku berikan semua 
+B          A 
+hanya untukmu hanya untukmu 
+ 
+E           F#m 
+jangan kau jangan pernah 
+C#m           D 
+jangan pergi jangan pernah pergi dariku 
+E           F#m 
+jangan kau jangan pernah 
+C#m             D 
+jangan pergi jangan pernah pergi dariku 
+ 
+B       A      B 
+hooo . . .  hooo jangan pernah pergi dariku 
+ 
+E 
+ bersama nafasku bersama nafasmu 
+C#m 
+ bersama tubuhku rebahkan jiwamu 
+A 
+ bersama denganmu arungi mimpiku
+`
+  },
+  {
+    title: `Kau khianati`,
+    body: `
+ [ intro ]  A Em 
+ 
+A             Em  
+terlalu cepat berlalu 
+     A          Em 
+menikmatimu memilikimu 
+A                Em 
+tak tentu setiap langkahku 
+      A               Em 
+sejak kau tinggalkan aku 
+ 
+ [ chorus ] 
+A                Em 
+aaahhh kini ku sadari 
+         D 
+kini kuyakini 
+             Dm 
+tlah kau khianati 
+ 
+ [ intro ]  F#m E D 2x  
+        A Em 2x D Dm 
+ 
+ 
+A             Em  
+terlalu cepat berlalu 
+      A               Em 
+sejak kau tinggalkan aku 
+ 
+ 
+ [ chorus ] 
+A                Em 
+aaahhh kini ku sadari 
+         D 
+kini kuyakini 
+             Dm 
+tlah kau khianati 
+ 
+ F#m  E             D 
+aku . . .  tlah kau khianati  4x
+`
+  },
+  {
+    title: `Sampai Kapan`,
+    body: `
+ [ intro ]  C Am C Am 
+ 
+   C     Dm     F 
+akukan datang untukmu 
+    C      Dm          G 
+disaat kau menginginkanku 
+     C      Dm    F 
+tapi kau pergi sesuka hatimu 
+   Dm            G   F  G 
+sampai kapan kau begini 
+ 
+ 
+ [ chorus ] 
+       C 
+sampai kapan kapan sampai kapan 
+          Am 
+kita bisa bertemu 
+       Dm 
+sampai kapan kapan sampai kapan 
+          G 
+kita bisa berdua 
+  F G  C 
+selama ini selama ini  
+              Am 
+ku ingin kita bersama 
+       Dm 
+selama ini selama ini  
+             G           F G  
+kuingin kita bercinta selamanya 
+ 
+ 
+ [ intro ]  C Am C Am 
+ 
+   C     Dm     F 
+akukan datang untukmu 
+    C      Dm        G 
+disaat kau merindukanku 
+     C      Dm    F 
+tapi kau pergi sesuka hatimu 
+   Dm            G   F  G 
+sampai kap 
+an kau begini 
+ 
+ [ chorus ] 
+       C 
+sampai kapan kapan sampai kapan 
+          Am 
+kita bisa bertemu 
+       Dm 
+sampai kapan kapan sampai kapan 
+          G 
+kita bisa berdua 
+  F G  C 
+selama ini selama ini  
+              Am 
+ku ingin kita bersama 
+       Dm 
+selama ini selama ini  
+             G          F G    
+kuingin kita bercinta selamanya 
+ 
+Dm           G 
+sampai kapan ku menanti 
+Em           Am 
+sampai kapan ku disini 
+Dm        G 
+bersamamu selamanya 
+ 
+ [ chorus ] 
+       C 
+sampai kapan kapan sampai kapan 
+          Am 
+kita bisa bertemu 
+       Dm 
+sampai kapan kapan sampai kapan 
+          G 
+kita bisa berdua 
+  F G  C 
+selama ini selama ini  
+              Am 
+ku ingin kita bersama 
+       Dm 
+selama ini selama ini  
+             G          F G   C 
+kuingin kita bercinta selamanya 
+ 
+ [ outro ]  C Am C Am C
+`
+  },
+  {
+    title: `Selagi Ku Mampu`,
+    body: `
+ [ intro ]  E A B - C - C#m B A B 
+        
+        E A E A 
+        E A B - C - C#m F# F#m B 
+ 
+    E                              A 
+selama selama selama ku sadar akan diriku 
+      E                                A 
+tak mau tak mau tak mau aku mengganggu dirimu 
+    E                        A 
+selama aku masih di sini selama aku masih berdiri 
+        E             B        A 
+seluruh ragaku kan tertuju padamu 
+ 
+       E         B          A        B         C#m 
+ijinkanku ijinkanku mencintaimu menyayangi dirimu 
+       F#        F#m B 
+sampai akhir hidupku 
+       E         B         A        B         C#m 
+ijinkanku ijinkanku menemanimu mewarnai hidupmu 
+       F#        F#m   B 
+sampai akhir hidupku selagi ku ,  selagi ku mampu 
+ 
+E B A B 
+    selagi ku mampu 
+C#m F# F#m B 
+         selagi ku mampu 
+E B A B 
+    selagi ku mampu 
+C#m F# F#m B 
+ 
+ 
+    E                              A 
+selama selama selama ku sadar akan diriku 
+    E                                  A 
+tak mau tak mau tak mau aku mengganggu dirimu 
+    E                        A 
+selama aku masih di sini selama aku masih berdiri 
+        E             B         A 
+seluruh ragaku kan tertuju padamu 
+ 
+       E         B          A        B         C#m 
+ijinkanku ijinkanku mencintaimu menyayangi dirimu 
+       F#        F#m B 
+sampai akhir hidupku 
+       E         B         A        B         C#m 
+ijinkanku ijinkanku menemanimu mewarnai hidupmu 
+       F#        F#m   B 
+sampai akhir hidupku selagi ku selagi ku mampu 
+ 
+ 
+       E         B          A             B      
+ijinkanku ijinkanku mencintaimu menyayangimu  ( selagi ku mampu )   
+       C#m       F#         F#m           B 
+ijinkanku ijinkanku mencintaimu menyayangimu  ( selagi ku mampu ) 
+       E         B          A             B      
+ijinkanku ijinkanku mencintaimu menyayangimu  ( selagi ku mampu )   
+       C#m       F#         F#m           B                  
+ijinkanku ijinkanku mencintaimu menyayangimu  ( selagi ku mampu ) 
+ 
+ [ outro ]  E
+`
+  },
+  {
+    title: `Selalu Kukenang`,
+    body: `
+ [ intro ]  E 
+ 
+E                G#m              A 
+mungkinkah aku telah terjatuh selamanya 
+E             C#m               A    B 
+selamanya aku takkan pernah melupakanmu 
+       A      B     E      A 
+engkau yang telah berikan ku 
+    A      B    A    B 
+harapan untukku impikan 
+ 
+ 
+ [ chorus ] 
+E             C#m     A            B     G#  C#m 
+genggamlah tanganku rasakan hangat lubuk hatiku 
+B           A 
+saat denganmu selalu ku kenang 
+E           C#m     A            B      G#  C#m 
+peganglah dada ku rasakan detak jantung hatiku 
+B           A    B             
+saat denganmu kaulah yang selalu ku kenang 
+ 
+ [ int ]  E 
+ 
+E                   G#m               A 
+mungkinkah engkau telah mencintai selamanya 
+E                C#m            A   B 
+selamanya engkau akan selalu di hatiku 
+       A      B     E     A 
+engkau yang telah berikan ku 
+    A      B    A   B 
+harapan untukku impikan 
+ 
+ 
+ [ chorus ] 
+E             C#m     A            B     G#  C#m 
+genggamlah tanganku rasakan hangat lubuk hatiku 
+B           A                     B 
+saat denganmu  ( kaulah yang selalu ku kenang ) 
+E           C#m     A            B      G#  C#m 
+peganglah dada ku rasakan detak jantung hatiku 
+B           A                     B 
+saat denganmu  ( kaulah yang selalu ku kenang ) 
+ 
+E             C#m     A            B     G#  C#m 
+genggamlah tanganku rasakan hangat lubuk hatiku 
+B           A                     B 
+saat denganmu  ( kaulah yang selalu ku kenang ) 
+E           C#m     A            B      G#  C#m 
+peganglah dada ku rasakan detak jantung hatiku 
+B           A                     B 
+saat denganmu  ( kaulah yang selalu ku kenang ) 
+   A               B  
+kaulah yang selalu ku kenang 
+ 
+ [ outro ]  E
+`
+  },
+  {
+    title: `Tak Kan Pernah Bisa`,
+    body: `
+ [ intro ]  A D 
+ 
+A                D 
+semakin dalam ku merindukanmu 
+A                D 
+semakin jauh kau pergi dariku 
+A               D 
+semakin jauh ku mencintaimu 
+A                 D 
+semakin dalam kau hancurkan aku 
+ 
+ 
+ [ chorus ] 
+ A D                  A 
+aku tak kan pernah bisa 
+                 D 
+tak kan pernah bisa 
+     A E      F#m D    A  E  
+bersama ,  bersama ,  bersamamu 
+                    D 
+ku tak kan pernah bisa 
+ 
+ [ intro ]  A D A D 
+ 
+A               D 
+semakin jauh ku mencintaimu 
+A                 D 
+semakin dalam kau hancurkan aku 
+ 
+ 
+     A E      F#m D    A    
+bersama ,  bersama ,  bersamamu 
+   E                 D 
+ku tak kan pernah bisa 
+     A E      F#m D    A    
+bersama ,  bersama ,  bersamamu 
+   E                 D 
+ku tak kan pernah bisa 
+                  A 
+tak kan pernah bisa 
+                  D 
+tak kan pernah bisa 
+                  A 
+tak kan pernah bisa 
+                  D 
+tak kan pernah bisa 
+                  A 
+tak kan pernah bisa 
+ 
+ [ interlude ]  A D 2x 
+ 
+     A        D        A 
+bersama ,  bersama ,  bersamamu 
+                     D 
+ku tak kan pernah bisa 
+     A        D        A 
+bersama ,  bersama ,  bersamamu 
+                     D 
+ku tak kan pernah bisa 
+                  A 
+tak kan pernah bisa 
+                  D 
+tak kan pernah bisa 
+                  A 
+tak kan pernah bisa
+`
+  },
+  {
+    title: `Wajahmu Indahkan`,
+    body: `
+ [ intro ]  E A 2x 
+ 
+  E          A   
+wajahmu terindah 
+  F#m           B 
+satu yang sempurna 
+  E          A 
+indahnya dunia 
+  F#m       B 
+ketika kau ada 
+ 
+        A              B 
+kuingin selalu dekat dihatimu 
+        A            B 
+kuingin selalu denganmu 
+ 
+ [ chorus1 ] 
+        E           A 
+di wajahmu di senyummu 
+              F#m             B 
+membuatku selalu membayangkanmu 
+          E             A 
+putih hatimu cantik dirimu 
+           F#m             B 
+kaulah dewiku wajahmu mengalihkan duniaku 
+ 
+ [ intro ]  E A 2x 
+ 
+ 
+ [ chorus2 ] 
+        E           A 
+di wajahmu di senyummu 
+              F#m             B 
+membuatku selalu membayangkanmu 
+          C#m           A 
+putih hatimu cantik dirimu 
+           F#m             B 
+kaulah dewiku wajahmu mengalihkan duniaku 
+ 
+ [ coda ]  E A 2x E
+`
+  },
+];
